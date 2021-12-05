70afcc3432c66f4480049c69879a5102f7e538e8
ShanteTorres <kennedy2110@gmail.com>
2021-12-04 20:07:32 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const divide = (x, y) => {
     return 2000 / 100;
 }
 
-const square = x => parameter1 * parameter2; 
+const square = x => x * x;
 
 
 const add = (parameter1, parameter2) => parameter1 + parameter2; 
